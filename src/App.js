751d432f9a7e197e79c361177ee297b8677229be
nicholasstine <nickstine1450@gmail.com>
2021-04-07 19:46:02 -0700

    Initial front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Snaip</h1>
+      <button>start impression</button>
+      <button>take or choose photo</button>
+      <button>Save picture</button>
+      <button>delete picture</button>
+      <button>delete impression</button>
     </div>
   );
 }
